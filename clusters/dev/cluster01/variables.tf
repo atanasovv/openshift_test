8b09f54b09d075348b9f92c0d2f1ac07ece17e3f
Vladislav Atanasov <32698871+atanasovv@users.noreply.github.com>
2025-05-21 22:45:00 +0300

    Create variables.tf

---
+++ clusters/dev/cluster01/variables.tf
@@ -0,0 +1,12 @@
+// This file is not used by the template directly but shows how Terraform variables would look
+variable "clusterName" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "version" {
+  type = string
+}
